54b71a9b134e1affb245eb5ffd36f34d830f216e
GabrielPaez <darkdelll@gmail.com>
2022-11-25 02:33:01 -0300

    cambios en el dom

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let valor2
 let cantidad
 let moneda1
 let moneda2
+let x = 0
 const monedas = [{ moneda: 'USD', valor: 1 },
 { moneda: 'EUR', valor: 1 },
 { moneda: 'ARS', valor: 159.72 },
@@ -19,18 +20,41 @@ function buscarmoneda() {
         }
     }
     resultado = (valor2 / valor1) * cantidad
-    return resultado
+    resultadofinal = resultado.toFixed(2)
+    return resultadofinal
 }
-
+function removeAllChilds(a)
+ {
+ var a=document.getElementById(a);
+ while(a.hasChildNodes())
+	a.removeChild(a.firstChild);	
+ }
+const contenedor = document.getElementById("res")
 const btn = document.getElementById("calcular")
 btn.addEventListener('click',(evt)=>{
     evt.preventDefault()
-    console.log(evt)
+    removeAllChilds("res")
     moneda1 = document.getElementById("monedaUno").value
     moneda2 = document.getElementById("monedaDos").value
     cantidad = document.getElementById("cantidad").value
     buscarmoneda()
+    let items= cantidad + " " + moneda1 + " a " + moneda2 + " = " +resultadofinal+moneda2
+    localStorage.setItem("resultados"+x,items)
+    x+=1
+    if(localStorage.length != 0){
+        for(let i=0; i<localStorage.length; i+=1 ){
+            const elementop = document.createElement("p")
+            elementop.innerHTML = localStorage.getItem("resultados"+i)
+            contenedor.appendChild(elementop)
+        }
+    }
 
 })
 
+const limpiar = document.getElementById("botonLimpiar")
+limpiar.addEventListener('click', (evt)=>{
+    localStorage.clear()
+    removeAllChilds("res")
+    x = 0
+})
 
